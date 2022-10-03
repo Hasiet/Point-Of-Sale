0402f8f56d9c08560a2ee4955bf7fa8d5546e004
hasie <hasie@DESKTOP-60IVCEN>
2022-10-03 15:37:45 +0600

    Add project files.

---
+++ POS/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace POS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Save = new System.Windows.Forms.Button();
+            this.listView = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.Delete = new System.Windows.Forms.Button();
+            this.IdLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Save
+            // 
+            this.Save.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.Save.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Save.Location = new System.Drawing.Point(131, 281);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(147, 66);
+            this.Save.TabIndex = 1;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView
+            // 
+            this.listView.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.listView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listView.GridLines = true;
+            this.listView.HideSelection = false;
+            this.listView.Location = new System.Drawing.Point(530, 30);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(400, 400);
+            this.listView.TabIndex = 2;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "ID";
+            this.columnHeader1.Width = 140;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Name";
+            this.columnHeader2.Width = 140;
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NameLabel.Location = new System.Drawing.Point(44, 132);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(102, 32);
+            this.NameLabel.TabIndex = 3;
+            this.NameLabel.Text = "Name:";
+            // 
+            // Delete
+            // 
+            this.Delete.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.Delete.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Delete.Location = new System.Drawing.Point(323, 281);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(147, 66);
+            this.Delete.TabIndex = 4;
+            this.Delete.Text = "Delete";
+            this.Delete.UseVisualStyleBackColor = true;
+            this.Delete.Click += new System.EventHandler(this.Delete_Click);
+            // 
+            // IdLabel
+            // 
+            this.IdLabel.AutoSize = true;
+            this.IdLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IdLabel.Location = new System.Drawing.Point(44, 63);
+            this.IdLabel.Name = "IdLabel";
+            this.IdLabel.Size = new System.Drawing.Size(52, 32);
+            this.IdLabel.TabIndex = 6;
+            this.IdLabel.Text = "ID:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(152, 57);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(225, 38);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(152, 126);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(225, 38);
+            this.textBox2.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label1.Location = new System.Drawing.Point(13, 427);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "version: 1.0.0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 453);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.IdLabel);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.NameLabel);
+            this.Controls.Add(this.listView);
+            this.Controls.Add(this.Save);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Test";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.Button Delete;
+        private System.Windows.Forms.Label IdLabel;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ POS/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace POS
+{
+    
+    public partial class Form1 : Form
+    {
+        SqlConnection PosDBcn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\POS\POS\PosDB.mdf;Integrated Security=True");
+        SqlDataAdapter DataAdapter;
+        DataSet DataSet;
+        DataTable table = new DataTable();
+        public Form1()
+        {
+            
+            InitializeComponent();
+            
+        }
+        public void ShowData()
+        {
+            PosDBcn.Open();
+            SqlCommand cmdLoadDATA = new SqlCommand("select * from PosTable", PosDBcn);
+            DataAdapter = new SqlDataAdapter(cmdLoadDATA);
+            DataSet = new DataSet();
+            DataAdapter.Fill(DataSet, "ViewTable");
+            PosDBcn.Close();
+            table = DataSet.Tables["ViewTable"];
+            int i;
+            for (i = 0; i < table.Rows.Count; i++)
+            {
+                listView.Items.Add(table.Rows[i].ItemArray[0].ToString());
+                listView.Items[i].SubItems.Add(table.Rows[i].ItemArray[1].ToString());
+            }
+            PosDBcn.Close();
+            listView.FullRowSelect = true;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string cmdInsert = "insert into PosTable(Id, Name) Values ('" + textBox1.Text + "','" + textBox2.Text + "')";
+            PosDBcn.Open();
+            try
+            {
+                SqlCommand cmdAdd = new SqlCommand(cmdInsert, PosDBcn);
+                cmdAdd.ExecuteNonQuery();
+                PosDBcn.Close();
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox1.Focus();
+                listView.Items.Clear();
+                ShowData();
+            }
+            catch
+            {
+                MessageBox.Show("Already in the list!");
+                listView.FullRowSelect = true;
+            }
+            PosDBcn.Close();
+            
+
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            PosDBcn.Open();
+            try 
+            {
+                
+                SqlCommand cmdDelete = new SqlCommand("DELETE PosTable WHERE Id = '" + listView.SelectedItems[0].Text + "'", PosDBcn);
+                cmdDelete.ExecuteNonQuery();
+                PosDBcn.Close();
+                listView.Items.Clear();
+                ShowData();
+            }
+            catch 
+            {
+                MessageBox.Show("You did't selected the item.");
+                this.Controls.Clear();
+                this.InitializeComponent();
+                PosDBcn.Close();
+                ShowData();
+                
+            }
+            
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ShowData();
+
+        }
+    }
+}
